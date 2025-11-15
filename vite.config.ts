e303c9cda84672200ca7b7834631a23cc434504a
luisa1031 <luisasalamanca.fc@academia.umb.edu.co>
2025-11-15 03:43:41 +0000

    mmm

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
-  import react from "@vitejs/plugin-react";
-  
-  // https://vitejs.dev/config/
-  export default defineConfig({
-      plugins: [react()],
-  });
-  
+import react from "@vitejs/plugin-react";
+const repoName = "/umb-frontend-react2/";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: repoName,
+  plugins: [react()],
+});
